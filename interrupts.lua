4fb017847fb08b42346f2e629d7d9014691f31d6
Ironi <jessemanelius@gmail.com>
2016-04-16 16:36:58 +0300

    add dispels.lua & interrupts.lua

---
+++ interrupts.lua
@@ -0,0 +1,42 @@
+local _, iEET = ...
+iEET.interrupts = {
+	--General
+	[155145] = true, --Arcane Torrent
+	
+	--Death Knight
+	[47476] = true, --Strangulate
+	[47528] = true, --Mind Freeze
+	
+	--Demon Hunter
+	[183752] = true, --Consume Magic
+	
+	--Druid
+	[97547] = true, --Solar Beam
+	[106839] = true, --Skull Bash
+	
+	--Hunter
+	[147362] = true, --Counter Shot
+	
+	--Mage
+	[2139] = true, --Counterspell
+	
+	--Monk
+	[116705] = true, --Spear Hand Strike
+	
+	--Paladin
+	[96231] = true, --Rebuke
+	
+	--Priest
+	[15487] = true, --Silence
+	
+	--Rogue
+	[1766] = true, --Kick
+	
+	--Warlock
+	[19647] = true, --Spell Lock (Felhunter)
+	[115781] = true, --Optical Blast (Observer)
+	[132409] = true, --Spell Lock (Grimoire of Sacrifice)
+	
+	--Warrior
+	[6552] = true, --Pummel	
+}
