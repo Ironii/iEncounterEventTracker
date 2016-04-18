6878396ad40f867604dfbee143fac5e987c689c9
Ironi <jessemanelius@gmail.com>
2016-04-19 02:46:01 +0300

    npcIgnoreList.lua: add few new npcs to the blacklist (priest, mage, hunter pets)

--- npcIgnoreList.lua
+++ npcIgnoreList.lua
@@ -8,7 +8,58 @@ iEET.npcIgnoreList = {
 	[97055] = true,		--Shambling Horror
 	[99773] = true,		--Bloodworm
 	[100876] = true,	--Val'kyr Battlemaiden
+
+--Druid
+	[1964] = true,		--Treant
+	[47649] = true, 	--Wild Mushroom
+	[54983] = true,		--Treant
+	[94852] = true, 	--Boomkin 4pc	
+
+--General
+	[35642] = true, 	--Jeeves
+	[77789] = true,		--Blingtron 5000
+	[92993] = true, 	--Burning Blade Banner
+
+--Hunter
+	[62005] = true, 	--Beast(?)
+	[86187] = true, 	--Beast
+	[90521] = true,		--Binding Shot
+	[94072] = true,		--Dark Minion
+	[95021] = true, 	--Felboar (hunter 4pc?)
+	[95582] = true,		--Beast
+	[109075] = true,	--Binding Shot Visual	
+
+--Mage
+	[31216] = true, 	--Mirror Image
+	[47243] = true,		--Mirror Image
+	[47244] = true,		--Mirror Image
+	[91710] = true,		--T18 (Archmage Khadgar)
+	[94879] = true, 	--T18 (Tyrande Whisperwind)
+	[94922] = true, 	--T18 (Lady Jaina Proudmoore)
+	[94925] = true,		--T18 (Lady Sylvanas Windrunner)
+	[94946] = true,		--T18 (Arthas Menethil)
+	[103636] = true,	--Arcane Familiar
 	
+--Monk	
+	[60849] = true, 	--Jade Serpent Statue
+	[63508] = true, 	--Xuen
+	[69680] = true,		--Storm Spirit
+	[69791] = true,		--Fire Spirit
+	[69792] = true,		--Earth Spirit
+	[73967] = true,		--Xuen
+	[78065] = true,		--Jade Serpent Statue
+
+--Priest
+	[19668] = true, 	--Shadowfiend
+	[62982] = true, 	--Mindbender
+	[65282] = true,		--Void Tendril
+	[67235] = true, 	--Shadowfiend
+	[67236] = true,		--Mindbender
+	[98167] = true,		--Void Tendril
+
+--Rogue	
+	[77726] = true,		--Shadow Reflection	
+
 --Shaman
 	[2523] = true, 		--Searing Totem
 	[2630] = true, 		--Earthbinding Totem
@@ -26,7 +77,7 @@ iEET.npcIgnoreList = {
 	[95061] = true, 	--Greater Fire Elemental 
 	[97285] = true,		--Wind Rush Totem
 	[100820] = true,	--Spirit Wolf
-	
+
 --Warlock
 	[11859] = true, 	--Doomguard
 	[17252] = true,		--Felguard
@@ -42,48 +93,4 @@ iEET.npcIgnoreList = {
 	[95469] = true,		--T18 Demo (Visicous Hellhound)
 	[99737] = true,		--Wild Imp
 	[103673] = true, 	--Beholder
-	
---Mage
-	[31216] = true, 	--Mirror Image
-	[47243] = true,		--Mirror Image
-	[47244] = true,		--Mirror Image
-	[91710] = true,		--T18 (Archmage Khadgar)
-	[94879] = true, 	--T18 (Tyrande Whisperwind)
-	[94922] = true, 	--T18 (Lady Jaina Proudmoore)
-	[94925] = true,		--T18 (Lady Sylvanas Windrunner)
-	[94946] = true,		--T18 (Arthas Menethil)
-	
---Monk	
-	[60849] = true, 	--Jade Serpent Statue
-	[63508] = true, 	--Xuen
-	[69680] = true,		--Storm Spirit
-	[69791] = true,		--Fire Spirit
-	[69792] = true,		--Earth Spirit
-	[73967] = true,		--Xuen
-	[78065] = true,		--Jade Serpent Statue
---Priest
-	[19668] = true, 	--Shadowfiend
-	[62982] = true, 	--Mindbender
-	[65282] = true,		--Void Tendril
-	[67235] = true, 	--Shadowfiend
-	[67236] = true,		--Mindbender
---Druid
-	[1964] = true,		--Treant
-	[47649] = true, 	--Wild Mushroom
-	[54983] = true,		--Treant
-	[94852] = true, 	--Boomkin 4pc
---Rogue	
-	[77726] = true,		--Shadow Reflection	
---Hunter
-	[62005] = true, 	--Beast(?)
-	[86187] = true, 	--Beast
-	[95021] = true, 	--Felboar (hunter 4pc?)
-	[90521] = true,		--Binding Shot
-	[94072] = true,		--Dark Minion
-	[95582] = true,		--Beast
-	
---General
-	[35642] = true, 	--Jeeves
-	[77789] = true,		--Blingtron 5000
-	[92993] = true, 	--Burning Blade Banner
 }
