baa66626da5955658630e7e84133dfc5dc54c0bd
Ironi <jessemanelius@gmail.com>
2016-05-22 04:05:28 +0300

    whitespace cleaning up

--- approvedSpells.lua
+++ approvedSpells.lua
@@ -1,6 +1,6 @@
 local _, iEET = ...
 iEET.approvedSpells = {
 --Priest
-	[605] = true, 	--Dominate Mind
+	[605] = true,	--Dominate Mind
 --Encounter Specific
-};
+}
--- dispels.lua
+++ dispels.lua
@@ -4,39 +4,39 @@ local _, iEET = ...
 -- 3 = Both
 iEET.dispels = {
 	--Druid
-	[2782] = 1, --Remove Corruption
-	[2908] = 2, --Soothe
-	[88423] = 1, --Nature's Cure
-	
+	[2782] = 1,		--Remove Corruption
+	[2908] = 2,		--Soothe
+	[88423] = 1,	--Nature's Cure
+
 	--Hunter
-	[19801] = 2, --Tranquilizing Shot
-	
+	[19801] = 2,	--Tranquilizing Shot
+
 	--Mage
-	[30449] = 2, --Spellsteal
-	[475] = 1, -- Remove Curse
-	
+	[30449] = 2,	--Spellsteal
+	[475] = 1,		--Remove Curse
+
 	--Monk
-	[115310] = 1, --Revival
-	[115450] = 1, --Detox
-	
+	[115310] = 1,	--Revival
+	[115450] = 1,	--Detox
+
 	--Paladin
-	[4987] = 1, --Cleanse
-	[213644] = 1, --Cleanse Toxins
-	
+	[4987] = 1,		--Cleanse
+	[213644] = 1,	--Cleanse Toxins
+
 	--Priest
-	[527] = 1, --Purify
-	[528] = 2, --Dispel Magic
-	[32375] = 3, --Mass Dispel
-	
+	[527] = 1,		--Purify
+	[528] = 2,		--Dispel Magic
+	[32375] = 3,	--Mass Dispel
+
 	--Rogue
-	[5938] = 2, -- Shiv
-	
+	[5938] = 2,		--Shiv
+
 	--Shaman
-	[370] = 2, --Purge
-	[51886] = 1, --Cleanse Spirit
-	[77130] = 1, --Purify Spirit
-	
+	[370] = 2,		--Purge
+	[51886] = 1,	--Cleanse Spirit
+	[77130] = 1,	--Purify Spirit
+
 	--Warlock
-	[89808] = 1, --Singe Magic (Imp)
-	[115276] = 1, --Sear Magic (Fel Imp)
+	[89808] = 1,	--Singe Magic (Imp)
+	[115276] = 1,	--Sear Magic (Fel Imp)
 }
